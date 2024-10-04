b244ef2ad7ff3da0b521329e3dc26bc46efaa253
Mauricio Salazar Mac <e.m.salazar.duque@tue.nl>
2024-10-04 21:05:53 +0200

    Update smart meter read

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-import numpy as np
+import requests
+import time
+# Define the URL of the REST-API server
+url = "http://192.168.2.11:82/smartmeter/api/read"
 
-random_data = np.random.randn(10)
-random_data += 2
+#%%
+while True:
+    try:
+        # Send a GET request to the server
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the response as JSON
+            json_data = response.json()
+            # print("Successfully retrieved data:")
+            # print(json_data)
+        else:
+            print(f"Failed to retrieve data. Status code: {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
+    print(f"Power: {float(json_data['PowerDelivered_total']):.3f}")
+    time.sleep(3)
